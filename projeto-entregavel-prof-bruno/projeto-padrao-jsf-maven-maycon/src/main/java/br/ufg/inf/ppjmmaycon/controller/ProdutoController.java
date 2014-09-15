480a3ec11076869de23ed74642e091bd2131233a
maycon <maycon@192.168.30.36>
2014-09-15 10:13:53 -0300

    A tela de cadastro de Produot ja foi feito as ações de cadastrar e remover.

--- projeto-entregavel-prof-bruno/projeto-padrao-jsf-maven-maycon/src/main/java/br/ufg/inf/ppjmmaycon/controller/ProdutoController.java
+++ projeto-entregavel-prof-bruno/projeto-padrao-jsf-maven-maycon/src/main/java/br/ufg/inf/ppjmmaycon/controller/ProdutoController.java
@@ -14,65 +14,72 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class ProdutoController {
-    
+
     private Integer idPrduto = 0; //Simula o auto-increment do banco de dados.
-    
+
     /* Tela */
     private ProdutoBean produtoBean;
     /* Entidade */
-    private Produto produto;        
+    private Produto produto;
     /* Lista de protutos  a serem exibidos na view */
     private List<Produto> listaProduto;
-        
-    
-    public ProdutoController() {        
+
+    public ProdutoController() {
         produtoBean = new ProdutoBean();
         produto = new Produto();
         listaProduto = new ArrayList<Produto>();
     }
-    
-    public ProdutoBean getGrupoProdutoBean() {
+
+    public ProdutoBean getProdutoBean() {
         return produtoBean;
     }
 
     public void setProdutoBean(ProdutoBean produtoBean) {
         this.produtoBean = produtoBean;
     }
-    
-    public void cadastrar() {       
-       produto = new Produto();
-       produtoBean.setId(idPrduto++);
-       produto.setId(produtoBean.getId());
-       produto.setDescricao(produtoBean.getDescricao());
-              
-       listaProduto.add(produto);
-       
-       System.out.println("Grupo de Produtos: " + produto.getDescricao());      
-       
-   }
-    public void listar() {        
-        System.out.println("Lista de Produtos: tamanho de " + listaProduto.size());
-        for(Produto produto : listaProduto) {
-            //System.out.println("Id: " + grupoProduto.getId() + " Descricao: " + grupoProduto.getDescricao());            
-            System.out.println("Id: " + " Descricao: " + produto.getDescricao());
-        }        
-    }
-    public void limpar() {        
-        listaProduto.clear();        
-    }
 
-    /**
-     * @return the listaGrupoProduto
-     */
     public List<Produto> getListaProduto() {
         return listaProduto;
     }
 
-    /**
-     * @param listaProduto the listaGrupoProduto to set
-     */
     public void setListaProduto(List<Produto> listaProduto) {
         this.listaProduto = listaProduto;
     }
+
+    public void cadastrar() {
+        produto = new Produto();
+        produtoBean.setId(idPrduto++);
+        produto.setId(produtoBean.getId());
+        produto.setDescricao(produtoBean.getDescricao());
+        produto.setLucro(produtoBean.getLucro());
+        produto.setNcm(produtoBean.getNcm());
+        produto.setPrecoCompra(produtoBean.getPrecoCompra());
+        produto.setValor(produtoBean.getValor());
+
+        System.out.println("Grupo de Produtos: " + produto.getDescricao());
+
+        listaProduto.add(produto);
+
+        produto = new Produto();
+    }
+
+    public void listar() {
+        System.out.println("Lista de Produtos: tamanho de " + listaProduto.size());
+        for (Produto produto : listaProduto) {
+            //System.out.println("Id: " + grupoProduto.getId() + " Descricao: " + grupoProduto.getDescricao());            
+            System.out.println("Id: " + " Descricao: " + produto.getDescricao());
+        }
+    }
     
+    public void limpar() {
+        listaProduto.clear();
+    }
+    
+    public void remover(Produto produto) {
+        listaProduto.remove(produto);
+    }
+    
+    public void editar(Produto produto) {
+        //Produto produtoTemp = listaProduto.
+    }
 }
